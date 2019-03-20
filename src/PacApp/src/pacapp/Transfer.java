022d6ac2c4cba7456d2962777d83306f6a0fabf3
asmenezes <asmenezes23@gmail.com>
2019-03-19 20:31:18 -0400

    Added Buttons to tranfer

--- src/PacApp/src/pacapp/Transfer.java
+++ src/PacApp/src/pacapp/Transfer.java
@@ -5,10 +5,43 @@
  */
 package pacapp;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
 /**
  *
  * @author andrewmenezes
  */
-public class Transfer {
-    
+public class Transfer extends PacApp {
+
+    public void pTrans(Stage stage) throws Exception {
+        AnchorPane tAnchor = new AnchorPane();
+
+        Insets inset = new Insets(-1, -1, -1, -1);
+
+        javafx.geometry.Insets bFillIn = new javafx.geometry.Insets(0);
+
+        CornerRadii bFillCR = new CornerRadii(0);
+
+        BackgroundFill buFill = new BackgroundFill(Paint.valueOf("TRANSPARENT"), bFillCR, bFillIn);
+        Background buBack = new Background(buFill);
+
+        Button sync = new Button("Sync Phone");       //Creates button with image
+        sync.backgroundProperty().set(buBack);         //adds transparent background
+        sync.setPadding(inset);
+
+        Button backup = new Button("Backup Phone");       //Creates button with image
+        backup.backgroundProperty().set(buBack);         //adds transparent background
+        backup.setPadding(inset);
+
+        Button Copy = new Button("Duplicate Phone");       //Creates button with image
+        Copy.backgroundProperty().set(buBack);         //adds transparent background
+
+    }
 }
